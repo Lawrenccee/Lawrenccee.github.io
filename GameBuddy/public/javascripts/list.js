974b52da4f557bb5957c2a468309441969a9daf8
Lawrence <lawrenccee@gmail.com>
2018-02-15 10:36:38 -0800

    add gamebuddy directory

---
+++ GameBuddy/public/javascripts/list.js
@@ -0,0 +1,48 @@
+export const makeStreamerList =  (users, userOrder) => {
+  const streamList = document.getElementById("stream-list");
+
+  while (streamList.firstChild) {
+    streamList.removeChild(streamList.firstChild);
+  }
+
+  userOrder.forEach((userId) => {
+    if (users[userId].login) {
+      let li = document.createElement("li");
+      let anchor = document.createElement("a");
+
+      anchor.href = `https://twitch.tv/${users[userId].login}`;      
+      anchor.target = `_blank`;
+      anchor.innerHTML = `${users[userId].displayName}`;
+
+      li.appendChild(anchor);
+      streamList.appendChild(li);
+    }
+  });
+};
+
+export const makeGameOptions = (games, gameOrder) => {
+  const gameOptions = document.getElementById("game-options");
+
+  while (gameOptions.firstChild) {
+    gameOptions.removeChild(gameOptions.firstChild);
+  }
+
+  let allStreams = document.createElement("option");
+
+  allStreams.value = "all";
+  allStreams.innerHTML = `--All Streams--`;
+  allStreams.selected = "selected";
+
+  gameOptions.appendChild(allStreams);
+
+  gameOrder.forEach((gameId) => {
+    let option = document.createElement("option");
+    
+    option.value = gameId;
+    option.innerHTML = `${games[gameId].name}`;
+
+    if (gameId) {
+      gameOptions.appendChild(option);
+    }
+  });
+};
